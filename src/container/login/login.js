65fc66e87dde357d46cb7d3aa52f54b4aba4ccbf
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2021-03-14 11:28:49 +0600

    added login component

---
+++ src/container/login/login.js
@@ -0,0 +1,68 @@
+import React, { Fragment } from 'react'
+import { Field, Form, Formik } from 'formik'
+import FormInput from '../../component/form-input/form-input'
+import Button from '../../component/button/button'
+import * as Yup from 'yup'
+import CardContent from '../../component/card/card-content'
+import Card from '../../component/card/card'
+
+import './style.css'
+
+const formValidation = Yup.object().shape(
+  {
+    password: Yup.string().required('Password is required').min(6, 'Too short').max(50, 'Too long')
+  }
+)
+
+const Login = (props) => {
+  return (
+    <Card className='login'>
+      <CardContent className="login__inner">
+        <div className="login__inner__logo">
+          <img src="/logo.svg" className="login__inner__logo__img" alt='logo'/>
+          <div className="login__inner__logo__title">Ashar.App</div>
+        </div>
+        <Formik
+          initialValues={{
+            login: '',
+            password: ''
+          }}
+          validationSchema={formValidation}
+          onSubmit={(values, { setErrors }) => {
+            props.login({ login: values.login, password: values.password, setErrors })
+          }}
+        >
+          {() => {
+            return (
+              <Fragment>
+                <Form className='login__inner__form'>
+                  <Field
+                    type="login"
+                    name="login"
+                    height="50px"
+                    component={FormInput}
+                    placeholder="Логин"
+                  />
+                    <Field
+                      type="password"
+                      name="password"
+                      height="50px"
+                      component={FormInput}
+                      placeholder="Пароль"
+                    />
+                    <Button primary type='submit' fullWidthOnMobile>Кирүү</Button>
+                </Form>
+                <div onClick={props.resetPassword} type='button'
+                        className='login__inner__forgot-password'>
+                  Парольду унуттуңузбу?
+                </div>
+              </Fragment>
+            )
+          }}
+        </Formik>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Login
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import Home from './container/home-page/container'
 import AddTerm from './container/home-page/terms/add-term'
 import TermDetailsContainer from './container/term-page/term-details-container'
 import TranslationSuggestionDetails from './container/home-page/terms/translation-suggestion-details'
+import Login from './container/login/login'
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home}/>
+        <Route path="/login" exact component={Login}/>
         <Route path="/add-term" exact component={AddTerm}/>
         <Route path="/term/:id" component={TermDetailsContainer}/>
         <Route path="/suggestion/:id" component={TranslationSuggestionDetails}/>
