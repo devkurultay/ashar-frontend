e7dcadc84200240e2c52d1f669433ac049fbb8d3
muratjumashev <jumasheff@gmail.com>
2021-03-14 09:02:48 +0600

    Remove unused imports

--- src/container/term-page/term-details.js
+++ src/container/term-page/term-details.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import OtherLangExample from '../common/other-lang-example'
 import TranslationSuggestion from '../common/translation-suggestion'
 
