73b223de88b75838acea9dcacca1622af6e5f71f
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2021-03-14 09:19:18 +0600

    fixed div styles

--- src/container/home-page/terms/request-item.js
+++ src/container/home-page/terms/request-item.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import { useHistory } from "react-router-dom"
 import OtherLangExample from '../../common/other-lang-example'
-
-import '../style.css'
 import CardContent from '../../../component/card/card-content'
 
+import '../style.css'
+
 const RequestItem = (props) => {
+  console.log(props)
   const history = useHistory()
   return (
-    <CardContent noBackground noPadding className="request-item" onClick={() => history.push('/term/'+props.id)}>
-      <p>{props.term}</p>
-      <CardContent className="request-item__langs-wrapper">
-        {props.other_lang_examples.map((t, i) => <OtherLangExample key={i} {...t}/>)}
-      </CardContent>
+    <CardContent noBackground noPadding className="request-item">
+      <div className="request-item__content" onClick={() => history.push('/term/'+props.id)}>
+        <p>{props.term}</p>
+        <CardContent className="request-item__langs-wrapper">
+          {props.other_lang_examples.map((t, i) => <OtherLangExample key={i} {...t}/>)}
+        </CardContent>
+      </div>
       <img src="/bookmark.svg" alt="bookmark"/>
     </CardContent>
   )
