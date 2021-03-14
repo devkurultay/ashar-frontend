e27e21c5a6078a5fa56f894200a127b0939c3bfe
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2021-03-14 10:24:12 +0600

    added new icons

--- src/container/common/translation-suggestion.js
+++ src/container/common/translation-suggestion.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import CardContent from '../../component/card/card-content'
+
+import './style.css'
 
 const TranslationSuggestion = (props) => {
   const history = useHistory()
   return (
-    <div className="terms-translation-suggestion" onClick={() => history.push('/suggestion/'+props.id)}>
-      <span>{props.translation}</span>
-      <span>Автор: {props.author}</span>
-    </div>
+    <CardContent className="terms-translation-suggestion" onClick={() => history.push('/suggestion/'+props.id)}>
+      <div className="terms-translation-suggestion__content">
+        <span className="terms-translation-suggestion__content__translation">{props.translation}</span>
+        <span className="terms-translation-suggestion__content__author"><span>Автор: </span>{props.author}</span>
+      </div>
+      <div className="terms-translation-suggestion__icons">
+        <div className="terms-translation-suggestion__icons__comment">
+          <img src="/chat-left-text.svg" alt="chat-left-text"/>
+          <span>0</span>
+        </div>
+        <div className="terms-translation-suggestion__icons__like">
+          <img src="/hand-thumbs-up.svg" alt="hand-thumbs-up"/>
+          <span>0</span>
+        </div>
+      </div>
+    </CardContent>
   )
 }
 
--- src/container/term-page/term-details.js
+++ src/container/term-page/term-details.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import OtherLangExample from '../common/other-lang-example'
 import TranslationSuggestion from '../common/translation-suggestion'
+import Card from '../../component/card/card'
+import CardContent from '../../component/card/card-content'
+
+import './style.css'
 
 const TermDetails = (props) => {
   const requests = props.requests || []
@@ -8,19 +12,21 @@ const TermDetails = (props) => {
   if (termObj && Object.keys(termObj).length) {
     const { term, description, other_lang_examples, translation_suggestions } = termObj
     return (
-      <div>
-        <h1>{term}</h1>
-        {other_lang_examples?.length && other_lang_examples.map(
-          (t, i) => <OtherLangExample key={i} {...t}/>
-        )}
-        <p>{description}</p>
-        {translation_suggestions?.length ? <>
-          <p>Сунушталган котормолор</p>
-          {translation_suggestions.map(
-            (s, i) => <TranslationSuggestion key={i} {...s} />
+      <Card className="term-details">
+        <CardContent className="term-details__inner">
+          <h1 className="term-details__title">{term}</h1>
+          {other_lang_examples?.length && other_lang_examples.map(
+            (t, i) => <OtherLangExample key={i} {...t}/>
           )}
-        </> : null}
-      </div>
+          <p>{description}</p>
+          {translation_suggestions?.length ? <>
+            <h3 className="request-list__title">Сунушталган котормолор</h3>
+            {translation_suggestions.map(
+              (s, i) => <TranslationSuggestion key={i} {...s} />
+            )}
+          </> : null}
+        </CardContent>
+      </Card>
     )
   }
   return <div>Error</div>
